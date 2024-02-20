02c8f82958f61e59b958773be50a96290e67ea78
Ting A Lin <linshaoting6@gmail.com>
2024-02-20 08:50:09 +0700

    water_container

--- algos/src/bfs.rs
+++ algos/src/bfs.rs
@@ -46,50 +46,50 @@ pub fn max_depth_iteration(root: Option<Rc<RefCell<TreeNode>>>) -> i32 {
     max_depth
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
 
-    #[test]
-    fn it_works() {
-        assert_eq!(max_depth_iteration(tree![3, 9, 20, null, null, 15, 7]), 3);
-    }
-}
+//     #[test]
+//     fn it_works() {
+//         assert_eq!(max_depth_iteration(tree![3, 9, 20, null, null, 15, 7]), 3);
+//     }
+// }
 
-pub fn to_tree(vec: Vec<Option<i32>>) -> Option<Rc<RefCell<TreeNode>>> {
-    use std::collections::VecDeque;
-    let head = Some(Rc::new(RefCell::new(TreeNode::new(vec[0].unwrap()))));
-    let mut queue = VecDeque::new();
-    queue.push_back(head.as_ref().unwrap().clone());
+// pub fn to_tree(vec: Vec<Option<i32>>) -> Option<Rc<RefCell<TreeNode>>> {
+//     use std::collections::VecDeque;
+//     let head = Some(Rc::new(RefCell::new(TreeNode::new(vec[0].unwrap()))));
+//     let mut queue = VecDeque::new();
+//     queue.push_back(head.as_ref().unwrap().clone());
 
-    for children in vec[1..].chunks(2) {
-        let parent = queue.pop_front().unwrap();
-        if let Some(v) = children[0] {
-            parent.borrow_mut().left = Some(Rc::new(RefCell::new(TreeNode::new(v))));
-            queue.push_back(parent.borrow().left.as_ref().unwrap().clone());
-        }
-        if children.len() > 1 {
-            if let Some(v) = children[1] {
-                parent.borrow_mut().right = Some(Rc::new(RefCell::new(TreeNode::new(v))));
-                queue.push_back(parent.borrow().right.as_ref().unwrap().clone());
-            }
-        }
-    }
-    head
-}
+//     for children in vec[1..].chunks(2) {
+//         let parent = queue.pop_front().unwrap();
+//         if let Some(v) = children[0] {
+//             parent.borrow_mut().left = Some(Rc::new(RefCell::new(TreeNode::new(v))));
+//             queue.push_back(parent.borrow().left.as_ref().unwrap().clone());
+//         }
+//         if children.len() > 1 {
+//             if let Some(v) = children[1] {
+//                 parent.borrow_mut().right = Some(Rc::new(RefCell::new(TreeNode::new(v))));
+//                 queue.push_back(parent.borrow().right.as_ref().unwrap().clone());
+//             }
+//         }
+//     }
+//     head
+// }
 
-#[macro_export]
-macro_rules! tree {
-    () => {
-        None
-    };
-    ($($e:expr),*) => {
-        {
-            let vec = vec![$(stringify!($e)), *];
-            let vec = vec.into_iter().map(|v| v.parse::<i32>().ok()).collect::<Vec<_>>();
-            to_tree(vec)
-        }
-    };
-    ($($e:expr,)*) => {(tree![$($e),*])};
+// #[macro_export]
+// macro_rules! tree {
+//     () => {
+//         None
+//     };
+//     ($($e:expr),*) => {
+//         {
+//             let vec = vec![$(stringify!($e)), *];
+//             let vec = vec.into_iter().map(|v| v.parse::<i32>().ok()).collect::<Vec<_>>();
+//             to_tree(vec)
+//         }
+//     };
+//     ($($e:expr,)*) => {(tree![$($e),*])};
 
-}
+// }
--- algos/src/lib.rs
+++ algos/src/lib.rs
@@ -3,3 +3,4 @@ mod dfs;
 mod linked_list;
 mod recusion;
 mod sum_of_three;
+mod container_with_most_water;
